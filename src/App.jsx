f10190fc8d719f310175d8f7e91c7d5ccd356a35
Espadv69 <aitorjob69@gmail.com>
2025-02-02 12:34:17 +0100

    Add Cart and useState import, add Cart shows

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
+
 import ProductList from './components/ProductList'
+import Cart from './components/Cart'
 
 const App = () => {
+  const [cart, setCart] = useState([])
+
+  const addToCart = (product) => {
+    setCart([...cart, product])
+  }
+
   return (
     <div>
       <h1>Simple React Store</h1>
-      <ProductList />
+      <ProductList addToCart={addToCart} />
+      <Cart cart={cart} />
     </div>
   )
 }
