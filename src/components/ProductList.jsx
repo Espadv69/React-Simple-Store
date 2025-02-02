5f8f13dc3b024463ad5cfdf7575b0deb9db03871
Espadv69 <aitorjob69@gmail.com>
2025-02-02 12:12:32 +0100

    Add function to add items to cart

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import { products } from '../data/products'
 
-const ProductList = () => {}
+const ProductList = () => {
+  const [cart, setCart] = useState([])
+
+  const addToCart = (product) => {
+    setCart([...cart, product])
+  }
+}
 
 export default ProductList
