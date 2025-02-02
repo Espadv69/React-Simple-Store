2ff2dfd2bc4d8dd0848f108734cb0682a35211c7
Espadv69 <aitorjob69@gmail.com>
2025-02-02 12:23:05 +0100

    Add ProductList component to App and shows it

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-const App = () => {}
+import ProductList from './components/ProductList'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Simple React Store</h1>
+      <ProductList />
+    </div>
+  )
+}
 
 export default App
