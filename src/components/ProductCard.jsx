af4de79085eb19ee2af16e92a40bce20b5ae213a
Espadv69 <aitorjob69@gmail.com>
2025-02-02 12:41:33 +0100

    Add Product card component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,21 @@
-const ProductCard = ({ product, addToCart }) => {}
+const ProductCard = ({ product, addToCart }) => {
+  return (
+    <div key={product.id} className="card__product-list">
+      <img
+        src={product.image}
+        alt={product.name}
+        className="image__product-list"
+      />
+      <h3 className="name__product-list">{product.name}</h3>
+      <p className="price__product-list">{product.price}</p>
+      <button
+        className="addBtn__product-list"
+        onClick={() => addToCart(product)}
+      >
+        Add to Cart
+      </button>
+    </div>
+  )
+}
 
 export default ProductCard
