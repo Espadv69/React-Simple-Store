111493f81013fc7a91e7cb8ab26485ea991797db
Espadv69 <aitorjob69@gmail.com>
2025-02-02 13:08:53 +0100

    Add key to product cards and import ProductList css

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,15 @@
 import ProductCard from './ProductCard'
 import { products } from '../data/products'
 
+import '../css/ProductList.css'
+
 const ProductList = ({ addToCart }) => {
   return (
     <div>
       <h2>Products</h2>
       <div className="container__product-list">
         {products.map((product) => (
-          <ProductCard product={product} addToCart={addToCart} />
+          <ProductCard key={product.id} product={product} addToCart={addToCart} />
         ))}
       </div>
     </div>
