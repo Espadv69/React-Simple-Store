f2a1b6c7c6fc773d6377fda55f2d33a15233dd06
Espadv69 <aitorjob69@gmail.com>
2025-02-02 11:48:59 +0100

    Add root route

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
