58ae84cf1c9b9d2694c412966506320fdf26f6ee
Espadv69 <aitorjob69@gmail.com>
2025-02-02 12:18:41 +0100

    Add Structure of ProductList and mapped products

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,6 +7,31 @@ const ProductList = () => {
   const addToCart = (product) => {
     setCart([...cart, product])
   }
+
+  return (
+    <div>
+      <h2>Products</h2>
+      <div className="container__product-list">
+        {products.map((product) => (
+          <div key={product.id} className="card__product-list">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="image__product-list"
+            />
+            <h3 className="name__product-list">{product.name}</h3>
+            <p className="price__product-list">{product.price}</p>
+            <button
+              className="addBtn__product-list"
+              onClick={() => addToCart(product)}
+            >
+              Add to Cart
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default ProductList
