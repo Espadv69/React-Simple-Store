f59485b28e6628968fcee36643bf265802c3f249
Espadv69 <aitorjob69@gmail.com>
2025-02-03 19:41:04 +0100

    Add Header, img and h1 into header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,10 @@
-const Header = () => {}
+const Header = () => {
+  return (
+    <header className="container__header">
+      <img src="" alt="" className="img__header" />
+      <h1 className="h1__header">Your Store Simulator</h1>
+    </header>
+  )
+}
 
 export default Header
