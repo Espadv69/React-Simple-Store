df562a6fd80844e7736c075a34d00ba67b966c9c
Espadv69 <aitorjob69@gmail.com>
2025-02-03 19:47:30 +0100

    Add header image and alt image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 const Header = () => {
   return (
     <header className="container__header">
-      <img src="" alt="" className="img__header" />
+      <img
+        src="/header_img.webp"
+        alt="It's an image of a store with several Tech devices"
+        className="img__header"
+      />
       <h1 className="h1__header">Your Store Simulator</h1>
     </header>
   )
